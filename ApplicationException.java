21d3e5874c790fe55b3d21caaabb538aca0351df
Vivek Ghosh <vivek3ghosh@gmail.com>
2020-10-15 00:53:12 +0530

    Added ApplicationException
    Generic custom exception useful for any spring REST application.

---
+++ ApplicationException.java
@@ -0,0 +1,56 @@
+import org.springframework.http.HttpStatus;
+
+public class ApplicationException extends RuntimeException {
+
+	private static final long serialVersionUID = 8159934445993571899L;
+
+	private String errorMessage;
+	private HttpStatus status;
+
+	private ApplicationException(ExceptionBuilder builder) {
+		this.errorMessage = builder.errorMessage;
+		this.status = builder.status;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public static class ExceptionBuilder {
+
+		private String errorMessage;
+		private HttpStatus status;
+
+		public ExceptionBuilder(String errorMessage, HttpStatus status) {
+			this.errorMessage = errorMessage;
+			this.status = status;
+		}
+
+		public ExceptionBuilder setErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+			return this;
+		}
+
+		public ExceptionBuilder setStatus(HttpStatus status) {
+			this.status = status;
+			return this;
+		}
+
+		public ApplicationException build() {
+			return new ApplicationException(this);
+		}
+	}
+
+}
--- Message.java
+++ Message.java
@@ -1,3 +1,5 @@
+package org.vrex.sprint.rngesus.dto;
+
 import java.io.Serializable;
 
 public class Message implements Serializable {
@@ -6,43 +8,48 @@ public class Message implements Serializable {
 
 	private String text;
 	private Object data;
-	
+
 	private Message(MessageBuilder builder) {
 		this.text = builder.text;
 		this.data = builder.data;
 	}
-	
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 	public Object getData() {
 		return data;
 	}
+
 	public void setData(Object data) {
 		this.data = data;
 	}
-	
+
 	public static class MessageBuilder {
-		
+
 		private String text;
 		private Object data;
+
 		public MessageBuilder(String text, Object data) {
-			super();
 			this.text = text;
 			this.data = data;
 		}
+
 		public MessageBuilder setText(String text) {
 			this.text = text;
 			return this;
 		}
+
 		public MessageBuilder setData(Object data) {
 			this.data = data;
 			return this;
 		}
-		
+
 		public Message build() {
 			return new Message(this);
 		}
