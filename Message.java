67e4138c278b4a44d221cea1641ff7d3d6d2dacb
Vivek Ghosh <vivek3ghosh@gmail.com>
2020-10-16 21:43:32 +0530

    Added Swagger folder
    Swagger depenencies along with version added

--- Message.java
+++ Message.java
@@ -1,7 +1,9 @@
-package org.vrex.sprint.rngesus.dto;
-
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class Message implements Serializable {
 
 	private static final long serialVersionUID = 7790620252680329776L;
